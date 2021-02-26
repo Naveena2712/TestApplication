7c4bd9e5b0b0779ce3957562a45b17b21b6d50fb
naveena2712 <naveenaanandhi@gmail.com>
2021-02-26 19:26:03 +0530

    database first Approach

---
+++ WebApplication3database/WebApplication3database/Controllers/TablenewController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+namespace WebApplication3database.Controllers
+{
+    public class TablenewController : Controller
+    {
+
+        naveenaEntities entities = new naveenaEntities();
+        // GET: Tablenew
+        public ActionResult Index()
+        {
+            return View(entities.Tablenews.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Tablenew tablenew)
+        {
+            if(ModelState.IsValid)
+            {
+                entities.Tablenews.Add(tablenew);
+                entities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tablenew);
+        }
+
+        public ActionResult Edit(int ID)
+        {
+            Tablenew tablenew = entities.Tablenews.Find(ID);
+
+            return View(tablenew);
+           // return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,NAME,SALARY,DOB,AGE")]Tablenew tablenew)
+        {
+            if(ModelState.IsValid)
+            {
+              
+                entities.Entry(tablenew).State = EntityState.Modified;
+                entities.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tablenew);
+        }
+        
+        public ActionResult Delete(int ID)
+        {
+
+            Tablenew tablenew = entities.Tablenews.Find(ID);
+            return View(tablenew);
+        }
+     
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int ID)
+        {
+            Tablenew tablenew = entities.Tablenews.Find(ID);
+            entities.Tablenews.Remove(tablenew);
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Details(int ID)
+        {
+            Tablenew tablenew = entities.Tablenews.Find(ID);
+
+            return View(tablenew);
+        }
+    }
+}
---
+++ WebApplication3database/WebApplication3database/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebApplication3database")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplication3database")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed565bb3-8cd8-4f78-80a6-4a3f4a70fe34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
